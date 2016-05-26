584a34bbd3ae4a292b5a866cf216f94d207c2f14
Darrell Henderson <darrell@thingable.com>
2016-05-26 11:33:12 -0500

    fix stop when buttons 1-4 are released

--- carble/carble.ino
+++ carble/carble.ino
@@ -158,34 +158,39 @@ void loop(void)
   if(packetbuffer[1] == 'B'){
     Serial.print ("Button"); Serial.print(num);
     boolean pressed = packetbuffer[3] - '0';
-    uint8_t button = packetbuffer[2];
+    uint8_t button = packetbuffer[2] - '0';
     if(pressed){
       Serial.println(" pressed");
-      if(button == '1'){
+      if(button == 1){
         // Button 1 stuff
-      }else if(button == '2'){
+      }else if(button == 2){
         // Button 2 stuff
-      }else if(button == '3'){
+      }else if(button == 3){
         // Button 3 stuff
-      }else if(button == '4'){
+      }else if(button == 4){
         // Button 4 stuff
-      }else if(button == '5'){      // Move forward?
+      }else if(button == 5){      // Move forward?
         move(1, 200, 0);
         move(2, 200, 0);
-      }else if(button == '6'){      // Move backward?
+      }else if(button == 6){      // Move backward?
         move(1, 200, 1);
         move(2, 200, 1);
-      }else if(button == '7'){      // Turn right?
+      }else if(button == 7){      // Turn right?
         move(1, 200, 0);
         move(2, 200, 1);
-      }else if(button == '8'){      // Turn left?
+      }else if(button == 8){      // Turn left?
         move(1, 200, 1);
         move(2, 200, 0);
       }
     }
     if(!pressed){
       Serial.println(" released");
-      stop();
+      if(button >= 5){            // if one of the arrow keys
+        stop();
+      } else {                    // if 1-4
+
+      }
+
     }
   }
 }
